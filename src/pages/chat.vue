157c9321520738735d2145ba0c21fcae23065529
yeyuhang <yeyuhang@appicplay.com>
2022-03-25 18:50:45 +0800

    update

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -1,13 +1,24 @@
 <!--
  * @Author       : Eug
  * @Date         : 2022-03-23 17:11:51
- * @LastEditTime : 2022-03-25 18:19:43
+ * @LastEditTime : 2022-03-25 18:50:12
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /github/micro-base/src/pages/chat.vue
 -->
 <template>
+  <!-- 线上 -->
   <micro-app
+    name="chat"
+    url="http://47.93.229.170:8081/base/chat/"
+    :data="data"
+    macro
+    baseRoute="/chat"
+    disableSandbox
+    inline
+  />
+  <!-- 本地 -->
+  <!-- <micro-app
     name="chat"
     url="http://127.0.0.1:8081/base/chat/"
     :data="data"
@@ -15,8 +26,7 @@
     baseRoute="/chat"
     disableSandbox
     inline
-  >
-  </micro-app>
+  /> -->
 </template>
 
 <script lang="ts" setup>
