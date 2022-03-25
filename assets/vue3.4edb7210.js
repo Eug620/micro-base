90904ff5d18509a88301a526d04ed37af7531005
Eug620 <68459780+Eug620@users.noreply.github.com>
2022-03-25 03:05:43 +0000

    Deploying to gh-pages from @ Eug620/micro-base@74161975f5d5c289c031b1a4d407ceb7a6fb0a6f 🚀

--- assets/vue3.4edb7210.js
+++
@@ -1,1 +0,0 @@
-import{d as a,r as e,o as s,c as o,k as r,l as t}from"./vendor.2bb8db13.js";const u={class:"vue3"},d=a({setup(a){const d=e({from:"来自基座的初始化数据"});return(a,e)=>(s(),o("div",u,[(s(),r(t("micro-app"),{name:"vue3",url:"http://127.0.0.1:8081/micro-app/vue3/",data:d.value,macro:"",baseRoute:"/vue3",disableSandbox:"",inline:""},null,8,["data"]))]))}});export{d as default};
