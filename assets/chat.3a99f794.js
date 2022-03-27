022420af95a6914a4badc788d00d7bf4385e13b9
YZ3531 <53846533+YZ3531@users.noreply.github.com>
2022-03-27 10:29:14 +0000

    Deploying to gh-pages from @ Eug620/micro-base@36300520489ecc4b5834c9164571ada1d2aad0e6 🚀

---
+++ assets/chat.3a99f794.js
@@ -0,0 +1,1 @@
+import{d as a,r as t,a as s,o as e,h as o}from"./vendor.a9900446.js";const r=a({setup(a){const r=t({from:"来自基座的初始化数据"});return(a,t)=>{const n=s("micro-app");return e(),o(n,{name:"chat",url:"http://47.93.229.170:8081/base/chat/",data:r.value,macro:"",baseRoute:"/chat",disableSandbox:"",inline:""},null,8,["data"])}}});export{r as default};
