0cd45cc39f4b467d384666a88995b8d410c5dc3e
Eug620 <68459780+Eug620@users.noreply.github.com>
2022-04-24 07:31:18 +0000

    Deploying to gh-pages from @ Eug620/micro-base@27553fa9b5ba759aa72893a60f2d9205f4d93dfa 🚀

---
+++ assets/index.02dd5370.js
@@ -0,0 +1,1 @@
+import{_ as a,a as r,o as e}from"./index.d54af0c6.js";var n=a({},[["render",function(a,n){return e(),r("div",null,"404")}]]);export{n as default};
--- assets/index.465b5590.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.3e2ae738.js";import{f as e,o}from"./vendor.059f172d.js";var a=r({},[["render",function(r,a){return o(),e("div",null,"404")}]]);export{a as default};
