66950126776bf905d7aa1d485b2e41ec71fb4b77
yeyuhang <yeyuhang@appicplay.com>
2022-04-24 15:12:16 +0800

    feat(all): first

---
+++ changelog-tools/format-changelog.js
@@ -0,0 +1,29 @@
+/* 
+ * @Author       : Eug
+ * @Date         : 2022-04-24 15:09:08
+ * @LastEditTime : 2022-04-24 15:09:09
+ * @LastEditors  : Eug
+ * @Descripttion : Descripttion
+ * @FilePath     : /github/micro-base/changelog-tools/format-changelog.js
+ */
+const replace = require('replace')
+
+// 去掉指导
+const reg_notable = /All.*guidelines./
+
+// 修改标题
+const reg_title = /Changelog/
+
+replace({
+  regex: reg_title,
+  replacement: 'Appic Changelog',
+  paths: ['CHANGELOG.md'],
+  recursive: true
+})
+
+replace({
+  regex: reg_notable,
+  replacement: 'Automatically Generate Appic Changelog, Remember not to modify changelog manually',
+  paths: ['CHANGELOG.md'],
+  recursive: true
+})
