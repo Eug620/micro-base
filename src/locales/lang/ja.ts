cb9c36a83212a277638f17aa595cb21640538054
yeyuhang <yeyuhang@appicplay.com>
2022-04-26 15:06:44 +0800

    feat(admin): 集成i8n国际化

---
+++ src/locales/lang/ja.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author       : Eug
+ * @Date         : 2022-04-26 14:24:28
+ * @LastEditTime : 2022-04-26 14:24:28
+ * @LastEditors  : Eug
+ * @Descripttion : Descripttion
+ * @FilePath     : /micro-base/src/locales/lang/ja.js
+ */
+export default {
+  message: {
+    hello: 'こんにちは、世界',
+  },
+};
