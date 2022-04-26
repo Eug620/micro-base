60947c586340c620d731b6bca12368f7ac1cc614
Eug620 <68459780+Eug620@users.noreply.github.com>
2022-04-26 07:09:10 +0000

    Deploying to gh-pages from @ Eug620/micro-base@a81c1354645d22bd7778d6708ea6129a1f0755dc 🚀

---
+++ assets/index.883c56fe.js
@@ -0,0 +1,1 @@
+import{_ as r,a,o as e}from"./index.c1796299.js";var n=r({},[["render",function(r,n){return e(),a("div",null,"404")}]]);export{n as default};
--- assets/index.8b2cccf6.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,a as r,o as e}from"./index.773c883a.js";var n=a({},[["render",function(a,n){return e(),r("div",null,"404")}]]);export{n as default};
