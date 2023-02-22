8205948fa325f56366ef480011c88317a53b4615
Eug620 <68459780+Eug620@users.noreply.github.com>
2023-02-22 06:33:54 +0000

    Deploying to gh-pages from @ Eug620/micro-base@c3b9a7efb52e22a737240eb803e51b8f8f1b6b0b 🚀

---
+++ assets/index.922e9571.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{o as e,e as o}from"./index.c32c6c85.js";var n=r({},[["render",function(r,n){return e(),o("div",null,"404")}]]);export{n as default};
