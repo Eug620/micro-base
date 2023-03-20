a412c91d3b090e9d1de927cbf10b6a5ec4dd7d8b
Eug620 <68459780+Eug620@users.noreply.github.com>
2023-03-20 06:22:07 +0000

    Deploying to gh-pages from @ Eug620/micro-base@634b4c55711a70c1ee91c5b11a04c83af61fb810 🚀

---
+++ assets/chat.106dc5c4.js
@@ -0,0 +1,1 @@
+import{p as a,r as s,v as t,x as e,y as r}from"./acro-design.6eda7891.js";const o=a({setup(a){const o=s({from:"来自基座的初始化数据"});return(a,s)=>{const n=t("micro-app");return e(),r(n,{name:"chat",url:"https://eug.asia:8081/base/chat/",data:o.value,macro:"",baseRoute:"/chat",disableSandbox:"",inline:""},null,8,["data"])}}});export{o as default};
