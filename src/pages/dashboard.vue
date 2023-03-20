3801408c6c5d8059c540b058a5429b875ae68f49
eug <yyh3531@163.com>
2023-03-20 17:11:10 +0800

    feat(dashboard): 动态获取数据

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -2,7 +2,7 @@
  * @Author       : eug yyh3531@163.com
  * @Date         : 2023-02-21 15:34:00
  * @LastEditors  : eug yyh3531@163.com
- * @LastEditTime : 2023-03-20 14:03:28
+ * @LastEditTime : 2023-03-20 17:09:42
  * @FilePath     : /micro-base/src/pages/dashboard.vue
  * @Description  : filename
  * 
@@ -19,40 +19,49 @@
 
 <script lang="ts" setup>
 import Typed from "typed.js";
-import { onMounted } from "vue";
-onMounted(() => {
-    var typed = new Typed('.dashboard-title', {
-        strings: [
-            "人这种东西，除了为了永不满足的欲望追求新的刺激之外，也是重视稀少价值之物的奇怪生物。",
-            "若批评不自由，则赞美无意义。",
-            "渔舟唱晚，响穷彭蠡之滨；雁阵惊寒，声断衡阳之浦。",
-            "要超越过去与悲伤，用坚强和笑容去开拓明天。",
-            "他们只是想保护自己的灵魂，结果，雪穗从不以真面目示人，亮司则至今仍在黑暗的通风管中徘徊。",
-            "一口饭掉一滴泪，一次咀嚼一个叹息。",
-            "生活中最重要的事情：不是胜利，而是努力；不是征服，而是奋力拼搏。",
-            "活在现实中的人是错的，而那些哭泣的人才是正确的，孤独的我们才有人类的样子。",
-            "人生就是一场旅行，没有终点的这条路，和爱着的人肩并肩，寻找着梦的所在。",
-            "不怕万人阻挡，就怕自己投降，将来的你一定会感激现在拼命的自己，自己选择的路，再艰难，跪着也要走完。",
-            "少年去游荡，中年想掘藏，老年做和尚。",
-            "世界上一切不利状况，都是因为当事人的能力不足。",
-            "祈祷奇迹，其实不如无尽的练习。",
-            "生命值得为之而战。",
-            "记忆，总会传承下去，无论是用何种方式。",
-            "如果你掉进了黑暗里，你能做的，不过是静心等待，直到你的双眼适应黑暗。"
-        ],
+import { onActivated, onMounted, ref } from "vue";
+import { onBeforeRouteLeave } from "vue-router";
+let stringsArray:any[] = []
+let typed: any = null
+let timeoutID: any = null
+const initTyped = () => {
+    typed = new Typed('.dashboard-title', {
+        strings: stringsArray,
         typeSpeed: 60,
-        loop: true,
+        // loop: true,
         cursorChar: ' /',
         // fadeOut: true,
         shuffle: true,
-        backDelay: 5000
+        // backDelay: 8000,
         // startDelay: 4000
+        onStringTyped: function () {
+            clearTimeout(timeoutID)
+            timeoutID = setTimeout(() => {
+                typed.destroy()
+                useResetTyped()
+            }, 9000)
+        }
     });
-
-    fetch('https://api.vvhan.com/api/joke?type=json').then(async res => {
+}
+const useResetTyped = () => {
+    fetch('https://api.vvhan.com/api/ian?type=json').then(async res => {
         let result = await res.json()
-        console.log(result);
+        stringsArray = [result.data.vhan]
+        initTyped()
     })
+}
+
+onMounted(() => {
+    useResetTyped()
+})
+
+onBeforeRouteLeave(() => {
+    typed.stop()
+    clearTimeout(timeoutID)
+})
+
+onActivated(() => {
+    typed?.reset()
 })
 
 </script>
@@ -60,7 +69,7 @@ onMounted(() => {
 <style lang="scss">
 .dashboard {
     &-title {
-        color: rgb(var(--danger-3));
+        color: rgb(var(--warning-3));
 
     }
 
