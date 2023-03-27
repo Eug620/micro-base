ef26cabd828e15caffb322525654b6ac0519ffc0
eug <yyh3531@163.com>
2023-03-27 17:50:18 +0800

    feat(dashboard): 星座

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -2,7 +2,7 @@
  * @Author       : eug yyh3531@163.com
  * @Date         : 2023-02-21 15:34:00
  * @LastEditors  : eug yyh3531@163.com
- * @LastEditTime : 2023-03-27 15:17:36
+ * @LastEditTime : 2023-03-27 17:48:51
  * @FilePath     : /micro-base/src/pages/dashboard.vue
  * @Description  : filename
  * 
@@ -63,14 +63,41 @@
             </a-col>
             <a-col :span="8">
                 <a-card :bordered="false" class="dashboard-container-card mb-2.5">
+                    <a-carousel v-model:current="moveCurrent" indicator-position="outer" animation-name="card"
+                        class="w-full h-5/6" :auto-play="true" indicator-type="dot" show-arrow="hover">
+                        <a-carousel-item class="cursor-pointer" v-for="item in doubanList" :key="item.title">
+                            <img :src="item?.info?.imgurl" class="h-full m-auto" @click="useToDouBan(item)" />
+                        </a-carousel-item>
+                    </a-carousel>
+                    <div class="h-1/6 text-center animate__animated animate__fadeIn">
+                        {{ doubanList[moveCurrent - 1] && doubanList[moveCurrent - 1].title }}
+                    </div>
                 </a-card>
             </a-col>
             <a-col :span="8">
                 <a-card :bordered="false" class="dashboard-container-card mb-2.5">
+
                 </a-card>
             </a-col>
-            <a-col :span="24">
+            <a-col :span="8">
+                <a-card :bordered="false" class="dashboard-container-card">
+                </a-card>
+            </a-col>
+            <a-col :span="16">
                 <a-card :bordered="false" class="dashboard-container-card ">
+                    <template #title>
+                        <a-space fill align="center">
+                            <a-select placeholder="Please select ..." v-model="type">
+                                <a-option :value="item.value" v-for="item in typeList" :key="item.value">{{ item.label
+                                }}</a-option>
+                            </a-select>
+                            <a-select placeholder="Please select ..." v-model="time">
+                                <a-option :value="item.value" v-for="item in timeList" :key="item.value">{{ item.label
+                                }}</a-option>
+                            </a-select>
+                        </a-space>
+                    </template>
+                    <a-descriptions class="h-3/4 w-full overflow-scroll" :data="data" fill column="1" />
                 </a-card>
             </a-col>
         </a-row>
@@ -82,7 +109,7 @@
 
 <script lang="ts" setup>
 import Typed from "typed.js";
-import { onActivated, onMounted, ref } from "vue";
+import { onActivated, onMounted, Ref, ref, watchEffect } from "vue";
 import { onBeforeRouteLeave } from "vue-router";
 import { ThemeEnum, LangEnum, SpecialEfficiencyEnum } from '@/enums/system';
 import { useSystemStore } from 'store/app';
@@ -94,7 +121,7 @@ let typedInstances: any = ref(null)
 let timeoutID: any = ref(null)
 const resetTyped = ref(false)
 const initTyped = () => {
-    typedInstances.value = new Typed('.dashboard-title', {
+    typedInstances.value = new Typed('.dashboard-container-title', {
         strings: [typedStrings.value],
         typeSpeed: 60,
         // loop: true,
@@ -142,13 +169,190 @@ onActivated(() => {
     }
 })
 
+
+const doubanList: Ref<any[]> = ref([])
+const moveCurrent = ref(1)
+const initDouBan = () => {
+    fetch('https://api.vvhan.com/api/douban').then(async (res: any) => {
+        const result: any = await res.json()
+        console.log(result);
+
+        doubanList.value = result.data
+    })
+}
+const useToDouBan = (item: any) => {
+    window.open(item?.info?.url)
+}
+
+initDouBan()
+
+
+// 星座
+const current: any = ref({})
+
+const typeList = ref([
+    {
+        label: '白羊座',
+        value: 'aries'
+    }, {
+        label: '金牛座',
+        value: 'taurus'
+    },
+    {
+        label: '双子座',
+        value: 'gemini'
+    },
+    {
+        label: '巨蟹座',
+        value: 'cancer'
+    },
+    {
+        label: '狮子座',
+        value: 'leo'
+    },
+    {
+        label: '处女座',
+        value: 'virgo'
+    },
+    {
+        label: '天秤座',
+        value: 'libra'
+    },
+    {
+        label: '天蝎座',
+        value: 'scorpio'
+    },
+    {
+        label: '射手座',
+        value: 'sagittarius'
+    },
+    {
+        label: '摩羯座',
+        value: 'capricorn'
+    },
+    {
+        label: '水瓶座',
+        value: 'aquarius'
+    },
+    {
+        label: '双鱼座',
+        value: 'pisces'
+    },
+])
+const timeList = ref([
+    {
+        label: '今天',
+        value: 'today'
+    },
+    {
+        label: '明天',
+        value: 'nextday'
+    },
+    {
+        label: '本周',
+        value: 'week'
+    },
+    {
+        label: '今年',
+        value: 'year'
+    },
+    {
+        label: '❤️',
+        value: 'love'
+    }
+])
+const type = ref('leo')
+const time = ref('today')
+const data = ref([])
+const useHoroscope = async () => {
+    try {
+        let result: any = await fetch(`https://api.vvhan.com/api/horoscope?type=${type.value}&time=${time.value}`)
+        const res: any = await result.json()
+        if (res.success) {
+            current.value = Object.assign(current.value, res.data)
+            data.value = Object.assign([], [
+                {
+                    label: '更新时间',
+                    value: current.value.time
+                },
+                {
+                    label: '综合运势',
+                    value: current.value.fortune.all
+                },
+                {
+                    label: '爱情运势',
+                    value: current.value.fortune.love
+                },
+                {
+                    label: '学业工作',
+                    value: current.value.fortune.work
+                },
+                {
+                    label: '财富运势',
+                    value: current.value.fortune.money
+                },
+                {
+                    label: '健康指数',
+                    value: current.value?.index?.health
+                },
+                {
+                    label: '商谈指数',
+                    value: current.value?.index?.discuss
+                },
+                {
+                    label: '幸运颜色',
+                    value: current.value.luckycolor
+                },
+                {
+                    label: '幸运数字',
+                    value: current.value.luckynumber
+                },
+                {
+                    label: '速配星座',
+                    value: current.value.luckyconstellation
+                },
+                {
+                    label: '短评',
+                    value: current.value.shortcomment
+                },
+                {
+                    label: '综合运势',
+                    value: current.value?.fortunetext?.all
+                },
+                {
+                    label: '爱情运势',
+                    value: current.value.fortunetext.love
+                },
+                {
+                    label: '学业工作',
+                    value: current.value.fortunetext.work
+                },
+                {
+                    label: '财富运势',
+                    value: current.value.fortunetext.money
+                },
+                {
+                    label: '健康运势',
+                    value: current.value.fortunetext.health
+                },
+            ])
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+watchEffect(() => useHoroscope())
 </script>
 
 <style lang="scss">
 .dashboard-container {
     &-card {
         height: calc((100vh - 1.25rem) / 3);
+
         // margin-bottom: 0.625rem;
+        .arco-card-body {
+            height: 100%;
+        }
     }
 
     &-title {
