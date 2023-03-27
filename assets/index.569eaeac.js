8d302339564503ba783b56dd8b31436c758abfd0
Eug620 <68459780+Eug620@users.noreply.github.com>
2023-03-27 09:53:10 +0000

    Deploying to gh-pages from @ Eug620/micro-base@ef26cabd828e15caffb322525654b6ac0519ffc0 🚀

--- assets/index.569eaeac.js
+++
@@ -1,1 +0,0 @@
-import{o as r,c as o}from"./acro-design.3a66f56e.js";var n=((r,o)=>{const n=r.__vccOpts||r;for(const[t,e]of o)n[t]=e;return n})({},[["render",function(n,t){return r(),o("div",null,"404")}]]);export{n as default};
